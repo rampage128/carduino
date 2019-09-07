97b8c274fbefce3be1a082b2c25384c049d20c6f
rampage128 <frederik.wolter@googlemail.com>
2019-09-08 00:02:59 +0200

    Add rate limiting as crude anti spam mechanism

--- serialpacket.h
+++ serialpacket.h
@@ -9,17 +9,22 @@ public:
 	SerialDataPacket(uint8_t type, uint8_t id) {
 		_id = id;
 		_type = type;
+		_lastSerializationTime = 0;
 	}
 
-	void serialize(Stream * serial) {
-		serial->write("{");
-		serial->write(_type);
-		serial->write(_id);
-		if (sizeof(_payload) > 0) {
-			serial->write(sizeof(_payload));
-			serial->write((byte*) &_payload, sizeof(_payload));
+	void serialize(Stream * serial, uint32_t rateLimit = 0) {
+		if (rateLimit < 1 || _lastSerializationTime == 0
+				|| rateLimit <= millis() - _lastSerializationTime) {
+			serial->write("{");
+			serial->write(_type);
+			serial->write(_id);
+			if (sizeof(_payload) > 0) {
+				serial->write(sizeof(_payload));
+				serial->write((byte*) &_payload, sizeof(_payload));
+			}
+			serial->write("}");
+			_lastSerializationTime = millis();
 		}
-		serial->write("}");
 	}
 
 	T *payload() {
@@ -32,6 +37,7 @@ public:
 private:
 	uint8_t _id;
 	uint8_t _type;
+	uint32_t _lastSerializationTime;
 	T _payload;
 };
 
